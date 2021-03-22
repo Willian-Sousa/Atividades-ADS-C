73234d6f399bfe9df15a7352ad5589742f0f25bd
Willian Sousa <wills2dry@gmail.com>
2021-03-22 21:25:10 +0900

    Update main.c
    Ajuste final para entrega.

--- main.c
+++ main.c
@@ -12,80 +12,84 @@
 
 int main()
 {
+
     //Declaracao das variaveis
     char nome_prod[30];
     int area, qnt, desconto_base;
     float preco_prod, valor_bruto;
     float taxa, desconto_total, valor_total;
 
-
-
     printf("        Loja Denki Eletronicos");
     printf("\n **************************************\n");
-    printf("*  |1|  Informatica                    *\n");
-    printf("*  |2|  Jogos                          *\n");
-    printf("*  |3|  Eletronicos                    *\n");
-    printf("*  |0|  Sair do programa               *\n");
+    printf("*  |1|  Informatica (5%% de desconto)  *\n");
+    printf("*  |2|  Jogos (8%% de desconto)        *\n");
+    printf("*  |3|  Eletronicos (10%% de desconto) *\n");
+    printf("*  |4|  Sair do programa              *\n");
     printf(" **************************************\n");
     printf("Selecione a area do produto: ");
     scanf("%d", &area);
+    fflush(stdin);
 
 
-    switch(area){
+  switch(area){
 
-    case 0: //Sair do menu
-        system("cls");
+    case 0:
+
         printf("Escolheu sair do menu.\n");
         exit(0);
         break;
 
     case 1: //Informatica
-        system("cls");
-        fflush(stdin);
+
+
         printf("\nA area de Informatica esta com 5%% de desconto!\n");
         printf("\n");
         desconto_base = 5;
         taxa = 0.05;
         break;
 
-    case 2: //Jogos
-        system("cls");
+    case 2: //jogos
+
         printf("\nA area de Jogos esta com 8%% de desconto!\n");
         printf("\n");
         desconto_base = 8;
         taxa = 0.08;
-        fflush(stdin);
         break;
 
     case 3: //Eletronicos
-        system("cls");
+
         printf("\nA area de Eletronicos esta com 10%% de desconto!\n");
         printf("\n");
         desconto_base = 10;
         taxa = 0.10;
-        fflush(stdin);
+        break;
+
+    case 4:
+
+        printf("Escolheu sair do menu.\n");
+        exit(0);
         break;
 
     default: // Se numero digitado for diferente dos acima citados.
-        system("cls");
-        printf("\n--------------Area invalida!----------------\n\n");
-        printf(" Tente novamente.\n\n");
-        return main();
-        break;
+       system("cls");
+       printf("\n--------------Area invalida!----------------\n\n");
+       printf(" Tente novamente.\n\n");
+       return main();
+       break;
       }
 
 
-    // Entrada de dados
+   // Entrada de dados
 
-    system("cls");
-    printf("\n\nDigite o nome do produto: ");
-    scanf("%s", &nome_prod);
+    printf("Digite o nome do produto: ");
+    gets(nome_prod);
     fflush(stdin);
-    printf("Digite o valor do produto: R$ ");
+    printf("\nDigite o valor do produto: R$ ");
     scanf("%f", &preco_prod);
     fflush(stdin);
-    printf("Digite a quantidade de produtos: ");
+    printf("\nDigite a quantidade de produtos: ");
     scanf("%i", &qnt);
+    fflush(stdin);
 
     //Calculos
 
@@ -94,7 +98,6 @@ int main()
     valor_total = valor_bruto - desconto_total;
 
     // Resultados
-
     system("cls");
     printf("----------------------------------------------------------------------\n");
     printf("                     Relacao de produtos\n");
@@ -102,10 +105,9 @@ int main()
     printf("  Produto\n\n");
     printf(" >> %s\n", nome_prod);
     printf("      R$%5.2f x %i :                                          R$%5.2f\n", preco_prod, qnt, valor_bruto);
-    printf("       Desconto (%i%%)                                        R$%5.2f\n\n", desconto_base, desconto_total);
+    printf("       Desconto (%i%%)                                         R$%5.2f\n\n", desconto_base, desconto_total);
     printf("----------------------------------------------------------------------\n\n");
-    printf("   Total:                                                   R$%5.2f\n\n", valor_total);
-    printf("----------------------------------------------------------------------\n\n");
+    printf("   Total:                                                    R$%5.2f\n\n", valor_total);
 
 
 
